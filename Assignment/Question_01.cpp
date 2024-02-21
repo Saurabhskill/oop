e81b5339d79363e1aa29a56c28f94d1b6e9f2e1e
Saurabh Kumar Yadav <132043474+saurabhmlexplorer@users.noreply.github.com>
2024-02-21 14:19:38 +0530

    Create Question_01.cpp

---
+++ Assignment/Question_01.cpp
@@ -0,0 +1,9 @@
+// Write a C++ program to display “Welcome to BCE Bakhityarpur” Program. 
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Welcome to BCE Bakhtiyarpur"; 
+    return 0;
+}
