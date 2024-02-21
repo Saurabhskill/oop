2ce1d77f693343ff8e18aba336f6025e4d4471fe
Saurabh Kumar Yadav <132043474+saurabhmlexplorer@users.noreply.github.com>
2024-02-21 14:24:42 +0530

    Update Question_02.cpp

--- Assignment/Question_02.cpp
+++ Assignment/Question_02.cpp
@@ -14,3 +14,20 @@ int main(){
     cout<<"Area of the circle is: "<<area;
     return 0;
 }
+
+
+/* // b. Convert Celsius to Fahrenheit
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    float temp1, temp2;
+    cout<<"Enter the temperature in celsius:"<<endl;
+    cin>>temp1;
+
+    temp2 = 9.0/5*temp1 + 32;
+    cout<<temp1<<" degree celsius is equals to "<<temp2<<" degree fahrenheit";
+    return 0;
+}
+*/
